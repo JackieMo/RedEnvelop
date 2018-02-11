613ee6a14d74ccc0cfcdb42913f747dbfde9f8f8
mozj <mozj2017@gmail.com>
2018-02-11 11:20:57 +0800

    提交项目工程

---
+++ RedEnvelopDylib/Class/WBSettingViewController.m
@@ -0,0 +1,194 @@
+//
+//  WBSettingViewController.m
+//  WeChatRedEnvelop
+//
+//  Created by 杨志超 on 2017/2/22.
+//  Copyright © 2017年 swiftyper. All rights reserved.
+//
+
+#import "WBSettingViewController.h"
+#import "WeChatRedEnvelop.h"
+#import "WBRedEnvelopConfig.h"
+#import <objc/objc-runtime.h>
+#import "WBMultiSelectGroupsViewController.h"
+
+@interface WBSettingViewController () <MultiSelectGroupsViewControllerDelegate>
+
+@property (nonatomic, strong) MMTableViewInfo *tableViewInfo;
+
+@end
+
+@implementation WBSettingViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        _tableViewInfo = [[objc_getClass("MMTableViewInfo") alloc] initWithFrame:[UIScreen mainScreen].bounds style:UITableViewStyleGrouped];
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self initTitle];
+    [self reloadTableData];
+    
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+
+    MMTableView *tableView = [self.tableViewInfo getTableView];
+    [self.view addSubview:tableView];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+    [self stopLoading];
+}
+
+- (void)initTitle {
+    self.title = @"微信小助手";
+    
+    [self.navigationController.navigationBar setTitleTextAttributes:@{NSFontAttributeName: [UIFont boldSystemFontOfSize:18.0]}];
+}
+
+- (void)reloadTableData {
+    [self.tableViewInfo clearAllSection];
+    
+    [self addBasicSettingSection];
+    [self addAdvanceSettingSection];
+  
+    MMTableView *tableView = [self.tableViewInfo getTableView];
+    [tableView reloadData];
+}
+
+#pragma mark - BasicSetting
+
+- (void)addBasicSettingSection {
+    MMTableViewSectionInfo *sectionInfo = [objc_getClass("MMTableViewSectionInfo") sectionInfoDefaut];
+    
+    [sectionInfo addCell:[self createAutoReceiveRedEnvelopCell]];
+    [sectionInfo addCell:[self createDelaySettingCell]];
+    
+    [self.tableViewInfo addSection:sectionInfo];
+}
+
+
+- (MMTableViewCellInfo *)createAutoReceiveRedEnvelopCell {
+    return [objc_getClass("MMTableViewCellInfo") switchCellForSel:@selector(switchRedEnvelop:) target:self title:@"自动抢红包" on:[WBRedEnvelopConfig sharedConfig].autoReceiveEnable];
+}
+
+- (MMTableViewCellInfo *)createDelaySettingCell {
+    NSInteger delaySeconds = [WBRedEnvelopConfig sharedConfig].delaySeconds;
+    NSString *delayString = delaySeconds == 0 ? @"不延迟" : [NSString stringWithFormat:@"%ld 秒", (long)delaySeconds];
+    
+    MMTableViewCellInfo *cellInfo;
+    if ([WBRedEnvelopConfig sharedConfig].autoReceiveEnable) {
+        cellInfo = [objc_getClass("MMTableViewCellInfo") normalCellForSel:@selector(settingDelay) target:self title:@"延迟抢红包" rightValue: delayString accessoryType:1];
+    } else {
+        cellInfo = [objc_getClass("MMTableViewCellInfo") normalCellForTitle:@"延迟抢红包" rightValue: @"抢红包已关闭"];
+    }
+    return cellInfo;
+}
+
+- (void)switchRedEnvelop:(UISwitch *)envelopSwitch {
+    [WBRedEnvelopConfig sharedConfig].autoReceiveEnable = envelopSwitch.on;
+    
+    [self reloadTableData];
+}
+
+- (void)settingDelay {
+    UIAlertView *alert = [UIAlertView new];
+    alert.title = @"延迟抢红包(秒)";
+    
+    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+    alert.delegate = self;
+    [alert addButtonWithTitle:@"取消"];
+    [alert addButtonWithTitle:@"确定"];
+    
+    [alert textFieldAtIndex:0].placeholder = @"延迟时长";
+    [alert textFieldAtIndex:0].keyboardType = UIKeyboardTypeNumberPad;
+    [alert show];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 1) {
+        NSString *delaySecondsString = [alertView textFieldAtIndex:0].text;
+        NSInteger delaySeconds = [delaySecondsString integerValue];
+        
+        [WBRedEnvelopConfig sharedConfig].delaySeconds = delaySeconds;
+        
+        [self reloadTableData];
+    }
+}
+
+#pragma mark - ProSetting
+- (void)addAdvanceSettingSection {
+    MMTableViewSectionInfo *sectionInfo = [objc_getClass("MMTableViewSectionInfo") sectionInfoHeader:@"高级功能"];
+    
+    [sectionInfo addCell:[self createReceiveSelfRedEnvelopCell]];
+    [sectionInfo addCell:[self createQueueCell]];
+    [sectionInfo addCell:[self createBlackListCell]];
+    [sectionInfo addCell:[self createAbortRemokeMessageCell]];
+  
+    [self.tableViewInfo addSection:sectionInfo];
+}
+
+- (MMTableViewCellInfo *)createReceiveSelfRedEnvelopCell {
+    return [objc_getClass("MMTableViewCellInfo") switchCellForSel:@selector(settingReceiveSelfRedEnvelop:) target:self title:@"抢自己发的红包" on:[WBRedEnvelopConfig sharedConfig].receiveSelfRedEnvelop];
+}
+
+- (MMTableViewCellInfo *)createQueueCell {
+    return [objc_getClass("MMTableViewCellInfo") switchCellForSel:@selector(settingReceiveByQueue:) target:self title:@"防止同时抢多个红包" on:[WBRedEnvelopConfig sharedConfig].serialReceive];
+}
+
+- (MMTableViewCellInfo *)createBlackListCell {
+    
+    if ([WBRedEnvelopConfig sharedConfig].blackList.count == 0) {
+        return [objc_getClass("MMTableViewCellInfo") normalCellForSel:@selector(showBlackList) target:self title:@"群聊过滤" rightValue:@"已关闭" accessoryType:1];
+    } else {
+        NSString *blackListCountStr = [NSString stringWithFormat:@"已选 %lu 个群", (unsigned long)[WBRedEnvelopConfig sharedConfig].blackList.count];
+        return [objc_getClass("MMTableViewCellInfo") normalCellForSel:@selector(showBlackList) target:self title:@"群聊过滤" rightValue:blackListCountStr accessoryType:1];
+    }
+    
+}
+
+- (MMTableViewSectionInfo *)createAbortRemokeMessageCell {
+    return [objc_getClass("MMTableViewCellInfo") switchCellForSel:@selector(settingMessageRevoke:) target:self title:@"消息防撤回" on:[WBRedEnvelopConfig sharedConfig].revokeEnable];
+}
+
+- (void)settingReceiveSelfRedEnvelop:(UISwitch *)receiveSwitch {
+    [WBRedEnvelopConfig sharedConfig].receiveSelfRedEnvelop = receiveSwitch.on;
+}
+
+- (void)settingReceiveByQueue:(UISwitch *)queueSwitch {
+    [WBRedEnvelopConfig sharedConfig].serialReceive = queueSwitch.on;
+}
+
+- (void)showBlackList {
+    WBMultiSelectGroupsViewController *contactsViewController = [[WBMultiSelectGroupsViewController alloc] initWithBlackList:[WBRedEnvelopConfig sharedConfig].blackList];
+    contactsViewController.delegate = self;
+    
+    MMUINavigationController *navigationController = [[objc_getClass("MMUINavigationController") alloc] initWithRootViewController:contactsViewController];
+    
+    [self presentViewController:navigationController animated:YES completion:nil];
+}
+
+- (void)settingMessageRevoke:(UISwitch *)revokeSwitch {
+    [WBRedEnvelopConfig sharedConfig].revokeEnable = revokeSwitch.on;
+}
+
+#pragma mark - MultiSelectGroupsViewControllerDelegate
+
+- (void)onMultiSelectGroupCancel {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (void)onMultiSelectGroupReturn:(NSArray *)arg1 {
+    [WBRedEnvelopConfig sharedConfig].blackList = arg1;
+    
+    [self reloadTableData];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+@end
